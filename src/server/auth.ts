3a3bba63f4c6eb38ce88dbd4adb3ed5ca2cb80ca
Shevil Mistry <shevilmistry001@gmail.com>
2024-03-29 22:16:36 +0530

    Update auth adding invitatioin

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -32,6 +32,7 @@ export async function setupAuth(env: AppEnv, app: HonoApp): Promise<AuthSystem> 
       logger: true,
     },
     usePassword: true,
+    useInvitation: true,
     strategies: [
       { callbacks, client: googleClient },
     ],
